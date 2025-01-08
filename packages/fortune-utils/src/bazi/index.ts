ce260d33d82bd50e79d3d3f125f2e70cfee95d23
banny-gao <104517120@qq.com>
2025-01-08 20:35:07 +0800

    feat: 地支掌诀位置与六合

--- packages/fortune-utils/src/bazi/index.ts
+++ packages/fortune-utils/src/bazi/index.ts
@@ -13,14 +13,14 @@ export type GanName = NameConst<typeof GAN_NAME>
 export const GAN_NAME = ['甲', '乙', '丙', '丁', '戊', '己', '庚', '辛', '壬', '癸'] as const
 
 /** 天干五合 */
-export const GAN_WUHE = [
+export const GAN_HE = [
   ['甲', '己', '土', '中正'],
   ['乙', '庚', '金', '仁义'],
   ['丙', '辛', '水', '威制'],
   ['丁', '壬', '木', '淫慝'],
   ['戊', '癸', '火', '无情'],
 ] as const
-export type GanHeDescription = (typeof GAN_WUHE)[number][3]
+export type GanHeDescription = (typeof GAN_HE)[number][3]
 export type GanHe = TargetField<{
   name: GanName
   description: GanHeDescription
@@ -28,15 +28,16 @@ export type GanHe = TargetField<{
 }>
 /** thisArg, 不可为箭头函数 */
 export function ganHe(this: Gan, target: Gan | GanName): GanHe | undefined {
-  const transform = ([_, _name2, hua, description]: string[]): Required<Omit<GanHe, keyof TargetField>> => ({
-    description: description as GanHeDescription,
-    hua: getWuXing(hua as WuXingName) as WuXing,
-  })
+  const transform = ([_, _name2, hua, description]: string[]): Required<Omit<GanHe, keyof TargetField>> =>
+    ({
+      description: description as GanHeDescription,
+      hua: getWuXing(hua as WuXingName) as WuXing,
+    }) as Required<Omit<GanHe, keyof TargetField>>
 
   return getRelation.call(this, {
     target,
     nameArray: [...GAN_NAME],
-    relationArray: GAN_WUHE.map(item => [...item]),
+    relationArray: GAN_HE.map(item => [...item]),
     transform,
   }) as GanHe
 }
@@ -119,6 +120,7 @@ export const gans: Gan[] = GAN_NAME.map((name, index) => {
 
   return gan
 })
+console.log('十天干：', gans)
 
 /** 十二地支 */
 export type ZhiName = NameConst<typeof ZHI_NAME>
@@ -126,6 +128,63 @@ export const ZHI_NAME = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未',
 /** 生肖 */
 export type AnimalName = NameConst<typeof ANIMAL_NAME>
 export const ANIMAL_NAME = ['鼠', '牛', '虎', '兔', '龙', '蛇', '马', '羊', '猴', '鸡', '狗', '猪'] as const
+/** 地理 */
+export type GeoName = NameConst<typeof GEO_NAME>
+export const GEO_NAME = ['墨池', '柳岸', '广谷', '琼林', '草泽', '大驿', '烽堠', '花园', '名都', '寺钟', '烧原', '悬河'] as const
+
+/** 掌诀 原点为中指末关节和无名指末关节中间
+ * 子: [1, 0] 丑: [-1, 0]
+ */
+export type FingerPosition = (typeof FINGER_POSITION)[number]
+export const FINGER_POSITION = [
+  [1, 0], // 子
+  [-1, 0], // 丑
+  [-3, 0], // 寅
+  [-3, 1], // 卯
+  [-3, 2], // 辰
+  [-3, 3], // 巳
+  [-1, 3], // 午
+  [1, 3], // 未
+  [3, 3], // 申
+  [3, 2], // 酉
+  [3, 1], // 戌
+  [3, 0], // 亥
+] as const
+
+/** 地支六合 */
+const ZHI_HE = [
+  ['子', '丑', '土/水', '泥合'],
+  ['寅', '亥', '木', '义合'],
+  ['卯', '戌', '火', '淫合'],
+  ['辰', '酉', '金', '融合'],
+  ['巳', '申', '水', '刑合'],
+  ['午', '未', '土/火', '和合'],
+] as const
+export type ZhiHeDescription = (typeof ZHI_HE)[number][3]
+export type ZhiHe = TargetField<{
+  name: ZhiName
+  targetName: ZhiName
+  hua: WuXing
+  huaTwo?: WuXing
+  description: ZhiHeDescription
+}>
+export function zhiHe(this: Zhi, target: Zhi | ZhiName): ZhiHe | undefined {
+  const transform = ([_, _name2, huas, description]: string[]): Required<Omit<ZhiHe, keyof TargetField>> => {
+    const [hua, huaTwo] = huas.split('/')
+    return {
+      description: description as ZhiHeDescription,
+      hua: getWuXing(hua as WuXingName) as WuXing,
+      huaTwo: huaTwo ? (getWuXing(huaTwo as WuXingName) as WuXing) : undefined,
+    } as Required<Omit<ZhiHe, keyof TargetField>>
+  }
+
+  return getRelation.call(this, {
+    target,
+    nameArray: [...ZHI_NAME],
+    relationArray: ZHI_HE.map(item => [...item]),
+    transform,
+  }) as ZhiHe
+}
 
 /** 四正|四旺（子午卯酉） */
 export type SizhengName = NameConst<typeof ZHENG_ZHI_NAME>
@@ -147,7 +206,6 @@ export const getZhiWuxing = (index: Zhi['index']): WuXing => {
   seasonIndex = seasonIndex >= 2 ? seasonIndex + 1 : seasonIndex
 
   const wuxingIndex = isTu ? 2 : seasonIndex
-  console.log(index, offsetIndex, isTu, wuxingIndex)
 
   return wuxings[wuxingIndex]
 }
@@ -165,6 +223,8 @@ export type Zhi = IndexField<{
   wuxing: WuXing
   season: SeasonName
   animal: AnimalName
+  HE: typeof zhiHe
+  he: ReturnType<typeof zhiHe>
 }>
 /** 十二地支 */
 export const zhis: Zhi[] = ZHI_NAME.map((name, index) => {
@@ -172,6 +232,7 @@ export const zhis: Zhi[] = ZHI_NAME.map((name, index) => {
     ...generateNamesProp(
       {
         animal: ANIMAL_NAME,
+        geo: GEO_NAME,
       },
       index
     ),
@@ -180,7 +241,10 @@ export const zhis: Zhi[] = ZHI_NAME.map((name, index) => {
     yinYang: yinYangs[(index + 1) % 2],
     wuxing: getZhiWuxing(index),
     season: [...SEASON_NAME][Math.floor(((index - 2 + 12) % 12) / 3)],
+    HE: zhiHe,
   } as Zhi
+  //  掌诀 横合 竖害 斜冲
+  zhi.he = zhiHe.call(zhi, ZHI_NAME[(zhi.index + 1) % 12])
 
   return zhi
 })
@@ -375,7 +439,6 @@ export interface Bazi {
 
 /** 获取八字 */
 export const getBazi = async (date: Date, address?: number | string): Promise<Bazi> => {
-  console.log('十天干：', gans)
   const lunarDate = await getSolarAndLunarDate(date, address)
   console.log('农历日期：', lunarDate)
   // 年柱
